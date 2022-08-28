3e6fbbcb6db2eba1599b81fe16d6dcdb3d4d582b
GrIvan1997 <69175067+GrIvan1997@users.noreply.github.com>
2022-08-28 17:04:58 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,40 @@
+const owl = $('.owl-carousel');
 
-  var owl = $('.owl-carousel');
 owl.owlCarousel({
-    center: true,
-    loop : true,
-    margin: 30,
-    startPosition: 1,
-    items: 3,
+	center: true,
+	loop: true,
+	margin: 20,
+	startPosition: 0,
+	items: 1,
+	responsive: {
+		540: {
+			items: 3,
+			startPosition: 1,
+		},
+		1200: {
+			items: 3,
+			margin: 30,
+		},
+	},
 });
-// Go to the next item
-$('.slider__btn--next').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.slider__btn--prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel');
-})
+
+$('.slider__btn--prev').click(function () {
+	owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn--next').click(function () {
+	owl.trigger('next.owl.carousel');
+});
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
